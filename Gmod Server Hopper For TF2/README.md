ce7d6097b66ca4781ed519e828f8204d80ee28a2
Dr. Lucy <42386612+drlucygreen@users.noreply.github.com>
2018-08-15 10:53:45 -0700

    Add files via upload

---
+++ Gmod Server Hopper For TF2/README.md
@@ -0,0 +1,6 @@
+Exho Server Hopping Bunny for Gmod changing but scripting it for TF2 sandbox Use 
+I have Exhos Personal Permission to edit this shell/script in anyway I so please :)
+-Dr. Lucy
+Discord: Dr. Lucy#4497
+
+Moddage™ Incorporation.
---
+++ Gmod Server Hopper For TF2/server-hopping-bunny/lua/shb/config.lua
@@ -0,0 +1,12 @@
+SHB.ScriptURL = "http://example.com/serverhopper" -- The location of the folder you uploaded to your webserver.
+SHB.ConsoleCommand = "serverhopper" -- Console command to open the Server Hopper menu.
+SHB.ChatCommand = "servers" -- Chat command to open the Server Hopper menu.
+
+SHB.Theme = "default" -- Check cl_themes.lua for information.
+
+SHB.Title = "New* Turtle Roleplay"
+
+-- Servers are added to the list like this:
+-- SHB.AddServer("ip", "port")
+
+SHB.AddServer("192.169.81.166", "27020")
